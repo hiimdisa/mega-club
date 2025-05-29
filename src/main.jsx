aaee6002dc814d881038a057e748842ec2875574
Disa <116721876+hiimdisa@users.noreply.github.com>
2025-05-29 20:45:42 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+)
+
+
+
+fix: add main.jsx
